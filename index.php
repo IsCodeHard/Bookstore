94081de06db8c4119720bbf7ca0d61351bb9d9d1
Brou Kouadio Stéphane-Fabien <bksf99@MacBook-Pro-de-Brou.local>
2023-05-01 15:50:12 +0000

    show products and single product

--- index.php
+++ index.php
@@ -1,10 +1,34 @@
 <?php include("./includes/header.php");?>
+<?php 
+    require("./config/config.php");
+
+    $req = $conn->query("SELECT * FROM products WHERE status = 1");
+    $req->execute();
+
+    $products=$req->fetchAll(PDO::FETCH_ASSOC);
+?>
 <main>
     <?php if(isset($_SESSION['bookstore'])):?>
-    <h4 class="username">Connected as : <?= $_SESSION['bookstore']['username'];?></h4>
+        <h4 class="username">Connected as : <?= $_SESSION['bookstore']['username'];?></h4>
     <?php endif;?>
-    <section id="articles">
-        
-    </section>
+    <hr>
+    <?php if(!empty($products)):?>
+        <section id="articles">
+            <?php foreach ($products as $book):?>
+                <div class="article">
+                    <div class="article_image_box">
+                        <img src="./img/<?= $book['image'];?>" alt="<?= $book['name'];?>">
+                    </div>
+                    <div class="article_content">
+                        <div class="article_name"><h4><?= $book['name'];?></h4><span>($<?= $book['price'];?>/item)</span></div>
+                        <p class="article_description"><?= strlen($book['description'])>80 ? substr($book['description'], 0, 80)."..." : $book['description'];?></p>
+                        <a href="/shopping/single.php?product_id=<?= $book['id']; ?>" class="btn btn_more">more -></a>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </section>
+    <?php else: ?>
+        <p class="empty" style="color:#ccc; text-align:center">No books available now!</p>
+    <?php endif; ?>
 </main>
 <?php include("./includes/footer.php");?>
--- shopping/single.php
+++ shopping/single.php
@@ -1,2 +1,35 @@
 <?php $title="single"; include("../includes/header.php");?>
+<?php
+    require("../config/config.php");
+
+    $product_id = strip_tags($_GET['product_id']) ?? "";
+
+    if(!empty($product_id)){
+        $retrieve = $conn->prepare("SELECT * FROM products WHERE id=:id AND status = 1");
+        $retrieve->execute(['id'=>$product_id]);
+
+        $book=$retrieve->fetch(PDO::FETCH_ASSOC);
+        if(!$book){
+            echo "<script>alert('no book found')</script>";
+        }
+    }else{
+        header('location:'.APPURL);
+    }
+?>
+<main id="main_single">
+    <?php if($book):?>
+        <section class="single">
+            <img src="../img/<?= $book['image'];?>" alt="">
+            <div class="single_content">
+                <a href="<?= APPURL;?>" class="btn"><- Back</a>
+                <h3 class="single_name"><?= $book['name'];?></h3>
+                <p class="single_price">$<?= $book['price'];?></p>
+                <p class="single_description"><?= $book['description'];?></p>
+                <a href="" class="btn"><- ADD TO CART</a>
+            </div>
+        </section>
+    <?php else:?>
+        <p class="empty" style="color:#ccc; text-align:center">No book found!</p>
+    <?php endif;?>
+</main>
 <?php include("../includes/footer.php");?>
